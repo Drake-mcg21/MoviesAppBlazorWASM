8a567f9fa3e261976fa06e75c09906f6bc6437c1
Drake McGillivray <drake.mcgillivray@powerhouse.ca>
2025-08-18 17:10:15 -0400

    Robot POC map

---
+++ MoviesApp.Client/Model/ServerModels.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace MoviesApp.Client.Model
+{
+    public enum EventType { All, Added, Updated, Removed }
+
+    public sealed class SignalREvent
+    {
+        public EventType Type { get; set; }
+        public JsonElement Payload { get; set; } // array or object
+    }
+
+    public record PositionDto(Guid RobotId, double X, double Y, double Yaw, DateTime Timestamp);
+}
